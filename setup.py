b83133366c645af38adf30445cfa142bebff5880
Paul Skopnik <paul@skopnik.me>
2020-01-23 18:34:48 +0100

    Add setup.py, README.md

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as file:
+	long_description = file.read()
+
+setup(
+	name="simulator",
+	version="0.1.dev0",
+	description="Simulates a HTC cache system",
+	author = "Paul Skopnik",
+	author_email = "paul@skopnik.me",
+	long_description = long_description,
+	long_description_content_type = 'text/markdown',
+	packages = find_packages(),
+	install_requires = [
+		'apq',
+		'bjec',
+	],
+	entry_points = {
+		'console_scripts': ['simulator=simulator.cli:main'],
+	},
+)
