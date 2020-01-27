0b73ece86bcc9f23c61a7f3869aaf5f9469a026c
Paul Skopnik <paul@skopnik.me>
2020-01-27 11:29:46 +0100

    Improve (B)MRC plotting code
     * Use new label_bytes and label_number_si functions of scales package
     * Fix inconsistencies between plots
     * Extend caption to include number of accesses, number of files

--- analysis/plot.R
+++ analysis/plot.R
@@ -1,10 +1,12 @@
 library(readr)
 library(dplyr)
 library(ggplot2)
-library(gdata)
+library(scales)
 
 function() {
-	d <- read_csv("two_months_eval.csv") %>%
+	source("./plot.R")
+
+	d <- read_csv("two_years_eval.csv") %>%
 		mutate(
 			theoretical_best_hit_rate = (accesses - files) / accesses,
 			theoretical_best_miss_rate = files / accesses,
@@ -20,55 +22,55 @@ function() {
 	width <- 8
 	height <- 5
 
+	accesses <- d %>% slice(1) %>% pull(accesses)
+	files <- d %>% slice(1) %>% pull(files)
 	total_bytes_accessed <- d %>% slice(1) %>% pull(total_bytes_accessed)
 	unique_bytes_accessed <- d %>% slice(1) %>% pull(unique_bytes_accessed)
 	theoretical_best_miss_rate <- d %>% slice(1) %>% pull(theoretical_best_miss_rate)
 	theoretical_best_byte_miss_rate <- d %>% slice(1) %>% pull(theoretical_best_byte_miss_rate)
 
-	caption_text <- paste(humanReadable(total_bytes_accessed), " accessed (", humanReadable(unique_bytes_accessed), " unique bytes). ", sep="")
+	format_bytes <- label_bytes(units="auto_binary", accuracy=0.1)
+	format_si <- label_number_si(accuracy=0.1)
 
-	library(scales)
+	caption_text <- paste(
+		format_bytes(total_bytes_accessed),
+		" accessed in ",
+		format_si(accesses),
+		" accesses (",
+		format_bytes(unique_bytes_accessed),
+		" unique bytes in ",
+		format_si(files),
+		" files).",
+		sep=""
+	)
 
 	d %>%
 		ggplot() +
 			geom_point(aes(x=storage_bytes, y=miss_rate, group=processor, colour=processor)) +
 			geom_line(aes(x=storage_bytes, y=miss_rate, group=processor, colour=processor)) +
 			geom_hline(yintercept=theoretical_best_miss_rate) +
-			scale_x_continuous(breaks=extended_breaks(Q=c(1, 1024)), labels=humanReadableLabs()) +
+			scale_x_continuous(breaks=extended_breaks(Q=c(1, 1024)), labels=format_bytes) +
 			labs(
-				x = "Cache Volume Size [Giga Byte]",
+				x = "Cache Volume Size",
 				y = "Miss Rate",
 				colour = "Algorithm",
 				caption = caption_text
 			)
 
-	ggsave("two_months_mrc.pdf", width=width, height=height)
+	ggsave("two_years_mrc.pdf", width=width, height=height)
 
 	d %>%
 		ggplot() +
-			geom_point(aes(x=storage_size, y=byte_miss_rate, group=processor, colour=processor)) +
-			geom_line(aes(x=storage_size, y=byte_miss_rate, group=processor, colour=processor)) +
+			geom_point(aes(x=storage_bytes, y=byte_miss_rate, group=processor, colour=processor)) +
+			geom_line(aes(x=storage_bytes, y=byte_miss_rate, group=processor, colour=processor)) +
 			geom_hline(yintercept=theoretical_best_byte_miss_rate) +
+			scale_x_continuous(breaks=extended_breaks(Q=c(1, 1024)), labels=format_bytes) +
 			labs(
-				x = "Cache Volume Size [Giga Byte]",
+				x = "Cache Volume Size",
 				y = "Byte Miss Rate",
 				colour = "Algorithm",
 				caption = caption_text
 			)
 
-	ggsave("two_months_bmrc.pdf", width=width, height=height)
-}
-
-humanReadableLabs <- function(units="auto", standard="IEC", digits=1, width=NULL, sep=" ", justify="right") {
-	function(x) {
-		sapply(x, function(val) {
-			if (is.na(val)) {
-				return("")
-			} else {
-				return(
-					humanReadable(val, units=units, standard=standard, digits=digits, width=width, sep=sep, justify=justify)
-				)
-			}
-		})
-	}
+	ggsave("two_years_bmrc.pdf", width=width, height=height)
 }
