cfe80dc73359670045d5d5e30aecaf3d878dd56f
Paul Skopnik <paul@skopnik.me>
2020-01-27 14:08:17 +0100

    Add docstrings to utils; Extend consume()

--- simulator/workload/utils.py
+++ simulator/workload/utils.py
@@ -8,6 +8,8 @@ from . import Job, Submitter, TimeStamp
 T = TypeVar('T')
 
 def ignore_args(f: Callable[[], T]) -> Callable[..., T]:
+	"""Returns a function which calls f while dropping all received arguments.
+	"""
 	def _inner(*args: Any, **kwargs: Any) -> T:
 		return f()
 
@@ -20,15 +22,29 @@ def ignore_args(f: Callable[[], T]) -> Callable[..., T]:
 # lambda _: f()
 
 def callback_iterable(cb: Callable[[], None]) -> Iterator[Any]:
+	"""Returns an iterator it which calls cb on next(it).
+	"""
 	return filter(lambda _: False, map(ignore_args(cb), [None]))
 
-def repeat_each(iterable: Iterable[T], n: int) -> Iterator[T]:
-	return itertools.chain.from_iterable(itertools.repeat(el, n) for el in iterable)
+def repeat_each(it: Iterable[T], n: int) -> Iterator[T]:
+	"""Returns an iterator which repeats each element of it n times.
+	"""
+	return itertools.chain.from_iterable(itertools.repeat(el, n) for el in it)
 
-def consume(it: Iterable[Any]) -> None:
-	collections.deque(it, maxlen=0)
+def consume(it: Iterable[Any], n: Optional[int]=None) -> None:
+	"""Advance the iterable it n-steps ahead. If n is None, consume entirely.
+
+	Copied from:
+	https://docs.python.org/3.7/library/itertools.html#itertools-recipes
+	"""
+	if n is None:
+		collections.deque(it, maxlen=0)
+	else:
+		next(islice(it, n, n), None)
 
 def accumulate_initial(iterable: Iterable[T], func: Callable[[T, T], T], initial: Optional[T]=None) -> Iterator[T]:
+	"""Like itertools.accumulate but yields initial first, if not None.
+	"""
 	initial_it = [initial] if initial is not None else []
 
 	return itertools.chain(
