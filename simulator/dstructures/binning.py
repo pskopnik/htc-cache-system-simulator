221ade28d0367634f86ec008cbba23a821827d1b
Paul Skopnik <paul@skopnik.me>
2020-02-05 16:43:43 +0100

    Add binning data structures and corresponding tests

---
+++ simulator/dstructures/binning.py
@@ -0,0 +1,247 @@
+import abc
+import itertools
+from typing import Callable, cast, Generic, Iterable, Iterator, List, Mapping, Set, Tuple, TypeVar
+
+from .sorted import SortedDefaultDict
+
+
+# TODO: Binner could also handle floats (numbers, limits)
+
+class Binner(abc.ABC):
+	@property
+	@abc.abstractmethod
+	def bins(self) -> int:
+		raise NotImplementedError
+
+	@abc.abstractmethod
+	def bin_edges(self) -> Iterator[int]:
+		raise NotImplementedError
+
+	@abc.abstractmethod
+	def bin_limits(self, bin: int) -> Tuple[int, int]:
+		raise NotImplementedError
+
+	@abc.abstractmethod
+	def __call__(self, num: int) -> int:
+		raise NotImplementedError
+
+
+class NoneBinner(Binner):
+	@property
+	def bins(self) -> int:
+		return -1
+
+	def bin_edges(self) -> Iterator[int]:
+		return iter(itertools.count())
+
+	def bin_limits(self, bin: int) -> Tuple[int, int]:
+		return bin, bin+1
+
+	def __call__(self, num: int) -> int:
+		return num
+
+
+class LogBinner(Binner):
+	def __init__(self, first: int=0, last: int=-1, step: int=1) -> None:
+		self._first: int = first
+		self._last: int = last
+		self._step: int = step
+
+		self._bins = -1 if last == -1 else (last - first) // step + 1
+
+		self._bin: Callable[[int], int]
+		if last == -1:
+			self._bin = lambda num: (max(num.bit_length() - 1, first) - first) // step
+		else:
+			self._bin = lambda num: (min(max(num.bit_length() - 1, first), last) - first) // step
+
+	@property
+	def bins(self) -> int:
+		return self._bins
+
+	def bin_edges(self) -> Iterator[int]:
+		it: Iterable[int]
+		if self._bins == -1:
+			it = itertools.count(start=self._first+self._step, step=self._step)
+		else:
+			it = range(self._first+self._step, self._last+1, self._step)
+
+		return itertools.chain((0,), (2 ** i for i in it))
+
+	def bin_limits(self, bin: int) -> Tuple[int, int]:
+		real_first = (2 ** (self._first + bin * self._step))
+		first: int
+		last: int
+
+		if bin == 0:
+			first = 0
+		else:
+			first = real_first
+
+		if self._last != -1 and bin == self._bins - 1:
+			past = -1
+		else:
+			past = real_first * 2 ** self._step
+
+		return first, past
+
+	def __call__(self, num: int) -> int:
+		return self._bin(num)
+
+
+_T_co = TypeVar('_T_co', covariant=True)
+_T_co_inner = TypeVar('_T_co_inner', covariant=True)
+
+class BinnedMapping(Generic[_T_co], Mapping[int, _T_co]):
+	class _ItemSet(Generic[_T_co_inner], Set[Tuple[int, _T_co_inner]]):
+		def __init__(self, mapping: 'BinnedMapping[_T_co_inner]') -> None:
+			self._mapping: BinnedMapping[_T_co_inner] = mapping
+
+		def __len__(self) -> int:
+			return len(self._mapping)
+
+		def __contains__(self, el: object) -> bool:
+			try:
+				el = cast('Tuple[int, _T_co_inner]', el)
+				if len(el) != 2:
+					return False
+				return self._mapping[el[0]] == el[1]
+			except (KeyError, TypeError):
+				return False
+
+		def __iter__(self) -> Iterator[Tuple[int, _T_co_inner]]:
+			container = self._mapping._container
+			binner = self._mapping._binner
+			cnst_get_el = self._mapping._construct_or_get_element
+			if self._mapping._binner.bins == -1:
+				return zip(
+					binner.bin_edges(),
+					itertools.chain(
+						container,
+						(cnst_get_el(i) for i in itertools.count(len(container))),
+					),
+				)
+			else:
+				return zip(binner.bin_edges(), container)
+
+	def __init__(
+		self,
+		binner: Binner,
+		default_factory: Callable[[], _T_co],
+	) -> None:
+		self._default_factory: Callable[[], _T_co] = default_factory
+
+		self._binner: Binner = binner
+		self._container: List[_T_co]
+		self._get_element: Callable[[int], _T_co]
+
+		if self._binner.bins != -1:
+			container = list(default_factory() for _ in range(self._binner.bins))
+			self._container = container
+			self._get_element = lambda bin: container[bin]
+		else:
+			self._container = []
+			self._get_element = self._construct_or_get_element
+
+	def _construct_or_get_element(self, bin: int) -> _T_co:
+		try:
+			return self._container[bin]
+		except IndexError:
+			default_factory = self._default_factory
+			it = (default_factory() for _ in range(bin - len(self._container) + 1))
+			self._container.extend(it)
+			return self._container[bin]
+
+	@property
+	def binner(self) -> Binner:
+		return self._binner
+
+	@property
+	def default_factory(self) -> Callable[[], _T_co]:
+		return self._default_factory
+
+	def __getitem__(self, num: int) -> _T_co:
+		return self._get_element(self._binner(num))
+
+	def __iter__(self) -> Iterator[int]:
+		return self._binner.bin_edges()
+
+	def __len__(self) -> int:
+		return self._binner.bins
+
+	def items(self) -> 'BinnedMapping._ItemSet[_T_co]':
+		return BinnedMapping._ItemSet(self)
+
+	def values_until(self, num: int, half_open: bool=True) -> Iterator[_T_co]:
+		last_bin = self._binner(num)
+		if not half_open:
+			last_bin += 1
+
+		if self._binner.bins == -1:
+			_ = self._construct_or_get_element(last_bin)
+
+		return itertools.islice(self._container, last_bin)
+
+	def values_from(self, num: int, half_open: bool=False) -> Iterator[_T_co]:
+		first_bin = self._binner(num)
+		if half_open:
+			first_bin += 1
+
+		if self._binner.bins == -1:
+			for i in range(first_bin, len(self._container)):
+				yield self._container[i]
+			for i in itertools.count(len(self._container)):
+				yield self._construct_or_get_element(i)
+		else:
+			for i in range(first_bin, self._binner.bins):
+				yield self._container[i]
+
+	def bin_limits(self, bin: int) -> Tuple[int, int]:
+		return self._binner.bin_limits(bin)
+
+	def bin_limits_from_num(self, num: int) -> Tuple[int, int]:
+		return self._binner.bin_limits(self._binner(num))
+
+
+class BinnedSparseMapping(Generic[_T_co], Mapping[int, _T_co]):
+	def __init__(
+		self,
+		binner: Binner,
+		default_factory: Callable[[], _T_co],
+	) -> None:
+		self._binner: Binner = binner
+		self._dict: SortedDefaultDict[int, _T_co] = SortedDefaultDict(default_factory)
+
+	@property
+	def binner(self) -> Binner:
+		return self._binner
+
+	@property
+	def default_factory(self) -> Callable[[], _T_co]:
+		return cast(Callable[[], _T_co], self._dict.default_factory)
+
+	def __getitem__(self, num: int) -> _T_co:
+		return self._dict[self._binner(num)]
+
+	def __iter__(self) -> Iterator[int]:
+		bin_limits = self._binner.bin_limits
+		return (bin_limits(bin)[0] for bin in self._dict.keys())
+
+	def __len__(self) -> int:
+		return len(self._dict)
+
+	def __delitem__(self, num: int) -> None:
+		del self._dict[self._binner(num)]
+
+	def items_until(self, num: int, half_open: bool=True) -> Iterator[Tuple[int, _T_co]]:
+		dct = self._dict
+		for key in dct.irange(maximum=self._binner(num), inclusive=(True, not half_open)):
+			yield key, dct[key]
+
+	def items_from(self, num: int, half_open: bool=False) -> Iterator[Tuple[int, _T_co]]:
+		dct = self._dict
+		for key in dct.irange(minimum=self._binner(num), inclusive=(not half_open, True)):
+			yield key, dct[key]
+
+	def bin_limits_from_num(self, num: int) -> Tuple[int, int]:
+		return self._binner.bin_limits(self._binner(num))
---
+++ tests/test_binning.py
@@ -0,0 +1,270 @@
+import itertools
+import pytest # type: ignore
+import random
+from typing import cast, List
+
+from simulator.dstructures.binning import (
+	BinnedMapping,
+	BinnedSparseMapping,
+	Binner,
+	LogBinner,
+	NoneBinner,
+)
+
+def _assert_binner_equals(binner: Binner, edges: List[int]) -> None:
+	assert binner.bins == len(edges)
+	assert list(binner.bin_edges()) == edges
+	limits = zip(edges, itertools.chain(edges[1:], (-1,)))
+	assert [binner.bin_limits(bin) for bin in range(binner.bins)] == list(limits)
+
+def _assert_bin_limits(binner: Binner, bins: int) -> None:
+	next_first = 0
+	for i in range(bins):
+		first, past = binner.bin_limits(i)
+		assert first == next_first
+		assert binner(first) == i
+		if past > 0:
+			assert binner(past - 1) == i
+			assert binner(past) == i + 1
+		next_first = past
+
+def _assert_bin_edges(binner: Binner, bins: int) -> None:
+	for i, edge in zip(range(bins), binner.bin_edges()):
+		assert binner(edge) == i
+		if i > 0:
+			assert binner(edge - 1) == i - 1
+
+def _assert_binning(binner: Binner, min: int, max: int, n: int=1000) -> None:
+	for num in (random.randrange(min, max) for _ in range(n)):
+		bin = binner(num)
+		assert bin >= 0
+		assert binner.bins == -1 or bin < binner.bins
+		bin_first, bin_past = binner.bin_limits(bin)
+		assert num >= bin_first
+		assert bin_past == -1 or num < bin_past
+
+def test_log_binner_basics() -> None:
+
+	# Test basic calculations by checking different step values with first=5
+	# and last=8.
+	#
+	#          2^5   2^6   2^7   2^8
+	#           32    64   128   256
+	# step=1  <     ][    ][    ][ >
+	# step=2  <           ][       >
+	# step=3  <                 ][ >
+	# step=4  <                    >
+
+	b = LogBinner(first=5, last=8, step=1)
+	_assert_binner_equals(b, [0, 64, 128, 256])
+
+	b = LogBinner(first=5, last=8, step=2)
+	_assert_binner_equals(b, [0, 128])
+
+	b = LogBinner(first=5, last=8, step=3)
+	_assert_binner_equals(b, [0, 256])
+
+	b = LogBinner(first=5, last=8, step=4)
+	_assert_binner_equals(b, [0])
+
+@pytest.mark.parametrize('step', (1, 2, 3, 4)) # type: ignore
+def test_log_binner_bounded(step: int) -> None:
+	b = LogBinner(first=10, last=40, step=step)
+
+	assert b.bins > 0
+
+	_assert_bin_limits(b, b.bins)
+	_assert_bin_edges(b, b.bins)
+	_assert_binning(b, 0, 2 ** 45)
+
+	num = random.randrange(2 ** 10, 2 ** 11)
+	for i in range(7):
+		old_bin = b(num)
+		num *= 2 ** step
+		assert b(num) == old_bin + 1
+
+@pytest.mark.parametrize('step', (1, 2, 3, 4)) # type: ignore
+def test_log_binner_unbounded(step: int) -> None:
+	b = LogBinner(first=10, step=step)
+
+	assert b.bins == -1
+
+	_assert_bin_limits(b, 1000)
+	_assert_bin_edges(b, 1000)
+	_assert_binning(b, 0, 2 ** 45)
+
+	num = random.randrange(2 ** 10, 2 ** 50)
+	for i in range(10):
+		old_bin = b(num)
+		num *= 2 ** step
+		assert b(num) == old_bin + 1
+
+def test_none_binner() -> None:
+	b = NoneBinner()
+
+	assert b.bins == -1
+
+	_assert_bin_limits(b, 1000)
+	_assert_bin_edges(b, 1000)
+	_assert_binning(b, 0, 2 ** 45)
+
+	for i in range(100):
+		num = random.randrange(0, 1000000)
+		assert num == b(num)
+
+# Following is an implementation of test_log_binner_unbounded and
+# test_none_binner using a parametrised fixture.
+
+@pytest.fixture(params=[ # type: ignore
+	(LogBinner, (1,), {}), # type: ignore
+	(LogBinner, (2,), {}),
+	(LogBinner, (3,), {}),
+	(LogBinner, (4,), {}),
+	(NoneBinner, tuple(), {}),
+])
+def unbounded_binner(request) -> Binner:
+	return cast(Binner, request.param[0](*request.param[1], **request.param[2]))
+
+def test_unbounded_binner(unbounded_binner: Binner) -> None:
+	assert unbounded_binner.bins == -1
+
+	_assert_bin_limits(unbounded_binner, 1000)
+	_assert_bin_edges(unbounded_binner, 1000)
+	_assert_binning(unbounded_binner, 0, 2 ** 45)
+
+def test_bounded_binned_mapping() -> None:
+	m: BinnedMapping[List[int]] = BinnedMapping(LogBinner(first=10, last=20, step=1), list)
+	b = m.binner
+
+	# test __len__()
+	assert len(m) == b.bins
+
+	# test __iter__()
+	assert list(m) == list(b.bin_edges())
+
+	# test keys()
+	assert list(m.keys()) == list(b.bin_edges())
+
+	# set-up: append edge value to each bin
+	for edge in b.bin_edges():
+		m[edge].append(edge)
+
+	# test __getitem__()
+	for edge in b.bin_edges():
+		assert m[edge] == [edge]
+
+	# test items()
+	assert list(m.items()) == list(zip(b.bin_edges(), map(lambda edge: [edge], b.bin_edges())))
+
+	# test values()
+	assert list(m.values()) == list(map(lambda edge: [edge], b.bin_edges()))
+
+	# test __getitem__() resolving correctly for bin limits
+	for bin in range(b.bins):
+		first, past = b.bin_limits(bin)
+		if past == -1:
+			continue
+		assert m[first] is m[past - 1]
+
+	# test values_until()
+	until_bin = b.bins // 2
+	until_num = b.bin_limits(until_bin)[0]
+	assert list(m.values_until(until_num, half_open=True)) == list(
+		map(lambda edge: [edge], itertools.islice(b.bin_edges(), until_bin)),
+	)
+	assert list(m.values_until(until_num, half_open=False)) == list(
+		map(lambda edge: [edge], itertools.islice(b.bin_edges(), until_bin + 1)),
+	)
+
+	# test values_from()
+	from_bin = b.bins // 2
+	from_num = b.bin_limits(from_bin)[0]
+	assert list(m.values_from(from_num, half_open=True)) == list(
+		map(lambda edge: [edge], itertools.islice(b.bin_edges(), from_bin + 1, None)),
+	)
+	assert list(m.values_from(from_num, half_open=False)) == list(
+		map(lambda edge: [edge], itertools.islice(b.bin_edges(), from_bin, None,)),
+	)
+
+	# test bin_limits()
+	for bin in range(b.bins):
+		assert m.bin_limits(bin) == b.bin_limits(bin)
+
+	# test bin_limits_from_num()
+	for bin, edge in enumerate(b.bin_edges()):
+		assert m.bin_limits_from_num(edge) == b.bin_limits(bin)
+
+def test_unbounded_binned_sparse_mapping() -> None:
+	m: BinnedSparseMapping[List[int]] = BinnedSparseMapping(LogBinner(first=10, step=1), list)
+	b = m.binner
+
+	# empty mapping
+
+	assert len(m) == 0
+	assert list(m) == []
+
+	# set-up: append edge value to some bins
+	m_edges = list(itertools.islice(b.bin_edges(), 0, 20, 4))
+	for edge in m_edges:
+		m[edge].append(edge)
+
+	# test __len__()
+	assert len(m) == len(m_edges)
+
+	# test __iter__()
+	assert list(m) == m_edges
+
+	# test keys()
+	assert list(m.keys()) == m_edges
+
+	# test __getitem__()
+	for edge in m_edges:
+		assert m[edge] == [edge]
+
+	# test items()
+	assert list(m.items()) == list(zip(m_edges, map(lambda edge: [edge], m_edges)))
+
+	# test values()
+	assert list(m.values()) == list(map(lambda edge: [edge], m_edges))
+
+	# test __getitem__() resolving correctly for bin limits
+	for edge in m_edges:
+		first, past = b.bin_limits(b(edge))
+		if past == -1:
+			continue
+		assert m[first] is m[past - 1]
+
+	# test items_until()
+	until_edge_ind = len(m_edges) // 2
+	until_num = m_edges[until_edge_ind]
+	assert list(m.items_until(until_num, half_open=True)) == list(zip(
+		map(lambda edge: b(edge), itertools.islice(m_edges, until_edge_ind)),
+		map(lambda edge: [edge], itertools.islice(m_edges, until_edge_ind)),
+	))
+	assert list(m.items_until(until_num, half_open=False)) == list(zip(
+		map(lambda edge: b(edge), itertools.islice(m_edges, until_edge_ind + 1)),
+		map(lambda edge: [edge], itertools.islice(m_edges, until_edge_ind + 1)),
+	))
+
+	# test items_from()
+	from_edge_ind = len(m_edges) // 2
+	from_num = m_edges[from_edge_ind]
+	assert list(m.items_from(from_num, half_open=True)) == list(zip(
+		map(lambda edge: b(edge), itertools.islice(m_edges, from_edge_ind + 1, None)),
+		map(lambda edge: [edge], itertools.islice(m_edges, from_edge_ind + 1, None)),
+	))
+	assert list(m.items_from(from_num, half_open=False)) == list(zip(
+		map(lambda edge: b(edge), itertools.islice(m_edges, from_edge_ind, None)),
+		map(lambda edge: [edge], itertools.islice(m_edges, from_edge_ind, None)),
+	))
+
+	# test bin_limits_from_num()
+	for edge in m_edges:
+		assert m.bin_limits_from_num(edge) == b.bin_limits(b(edge))
+
+	# test __delitem__()
+	for edge in m_edges[:len(m_edges)//2]:
+		del m[edge]
+	m_edges = m_edges[len(m_edges)//2:]
+	assert len(m) == len(m_edges)
+	assert list(m) == m_edges
