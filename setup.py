a762e681d4fe8d4bb5b41a8ddaa7386d214b5d41
Paul Skopnik <paul@skopnik.me>
2020-03-21 10:30:25 +0100

    Fix indentations in setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,28 @@
 from setuptools import setup, find_packages
 
 with open('README.md') as file:
-	long_description = file.read()
+    long_description = file.read()
 
 setup(
-	name="simulator",
-	version="0.1.dev0",
-	description="Simulates a HTC cache system",
-	author = "Paul Skopnik",
-	author_email = "paul@skopnik.me",
-	long_description = long_description,
-	long_description_content_type = 'text/markdown',
-	package_data = {'simulator': ['py.typed']},
-	packages = find_packages('src'),
-	package_dir = {'': 'src'},
-	zip_safe = False,
-	install_requires = [
-		'apq >= 0.10.0, < 0.20.0',
-		'bjec',
-		'orjson >= 2.5.1, < 3.0.0',
-		'typing-extensions',
-	],
-	entry_points = {
-		'console_scripts': ['simulator=simulator.cli:main'],
-	},
+    name = "simulator",
+    version = "0.1.dev0",
+    description = "Simulates a cache system in a high-throughput computing environment",
+    author = "Paul Skopnik",
+    author_email = "paul@skopnik.me",
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    package_data = {'simulator': ['py.typed']},
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+    zip_safe = False,
+    python_requires = '>= 3.7, < 4',
+    install_requires = [
+        'apq >= 0.10.0, < 0.20.0',
+        'bjec',
+        'orjson >= 2.5.1, < 3.0.0',
+        'typing-extensions',
+    ],
+    entry_points = {
+        'console_scripts': ['simulator=simulator.cli:main'],
+    },
 )
